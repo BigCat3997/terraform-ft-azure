426e259ce9498eaf3e4f2a9bf86c6b270dd30ef9
bigcat3997 <vqt.develop@gmail.com>
2023-08-06 22:31:25 +0700

    module to create cosmosdb simple

---
+++ resources/modules/cosmosdb_account/variable.tf
@@ -0,0 +1,36 @@
+variable "resource_group" {
+  type = object({
+    name     = string
+    location = string
+  })
+  description = <<EOT
+    resource_group = {
+      name : "a name of resource group"
+      location : "a location of resource group"
+    }
+  EOT
+}
+
+variable "cosmosdb_account_name" {
+  type        = string
+  description = "a name of cosmos database account"
+}
+
+variable "configuration" {
+  type = object({
+    free_tier              = bool
+    total_throughput_limit = number
+  })
+
+  default = {
+    free_tier              = true
+    total_throughput_limit = 1000
+  }
+
+  description = <<EOT
+    configuration = {
+      free_tier : "is this resource enable free tier"
+      total_throughput_limit : "number of throughput limit"
+    }
+  EOT
+}
